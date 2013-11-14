c2402edfb5dc4b3fddf1d5bd089a17857ffa0aa2
Rachel Lau <rachel.yw.lau@gmail.com>
2013-11-14 14:43:17 -0800

    Added test for demux

--- src/test/scala/simulations/CircuitSuite.scala
+++ src/test/scala/simulations/CircuitSuite.scala
@@ -80,4 +80,24 @@ class CircuitSuite extends CircuitSimulator with FunSuite {
     run
     assert(out.getSignal === true, "in1=true in2=true")
   }
+
+  test("demux example") {
+    val in, c0, c1, c2 = new Wire
+    val o0, o1, o2, o3, o4, o5, o6, o7 = new Wire
+    val c = List(c2, c1, c0)
+    val out = List(o7, o6, o5, o4, o3, o2, o1, o0) 
+    in.setSignal(true)
+    c2.setSignal(true)
+    c1.setSignal(false)
+    c0.setSignal(true)
+    demux(in, c, out)
+    assert(out(0).getSignal == false, "o7=false")
+    assert(out(1).getSignal == false, "o6=false")
+    assert(out(2).getSignal == true, "o5=true")
+    assert(out(3).getSignal == false, "o4=false")
+    assert(out(4).getSignal == false, "o3=false")
+    assert(out(5).getSignal == false, "o2=false")
+    assert(out(6).getSignal == false, "o1=false")
+    assert(out(7).getSignal == false, "o0=false")
+  }
 }
