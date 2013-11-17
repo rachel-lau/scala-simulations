ffc640140a355323ab43ecf97dfb4fda20ceeef5
Rachel Lau <rachel.yw.lau@gmail.com>
2013-11-17 11:19:31 -0800

    Populated persons

--- src/main/scala/simulations/EpidemySimulator.scala
+++ src/main/scala/simulations/EpidemySimulator.scala
@@ -12,11 +12,17 @@ class EpidemySimulator extends Simulator {
     val roomColumns: Int = 8
 
     // to complete: additional parameters of simulation
+    val prevalenceRate = 0.01
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val persons: List[Person] = {
+    val numInfected = (SimConfig.population * SimConfig.prevalenceRate).toInt
+    val result = (0 until SimConfig.population).toList.map(id => new Person(id))
+    for (id <- 0 until numInfected) result(id).infected = true
+    result
+  }
 
   class Person (val id: Int) {
     var infected = false
